382a8b7b1482b6044a6d1653eabbde5881d0e0b1
Thomas Chen <thomasthechen1@gmail.com>
2025-05-13 17:20:00 -0700

    changed model to gpt4.1, and updated prefs

--- filter_papers.py
+++ filter_papers.py
@@ -1,4 +1,4 @@
-import configparser
+filter_papers.pyimport configparser
 import dataclasses
 import json
 import re
@@ -52,10 +52,14 @@ def filter_papers_by_hindex(all_authors, papers, config):
 
 
 def calc_price(model, usage):
+    if model == 'gpt-4.1-mini':
+        return (0.40 * usage.prompt_tokens + 1.60 * usage.completion_tokens) / 1000000.0
     if model == "gpt-4-1106-preview":
         return (0.01 * usage.prompt_tokens + 0.03 * usage.completion_tokens) / 1000.0
     if model == "gpt-4":
         return (0.03 * usage.prompt_tokens + 0.06 * usage.completion_tokens) / 1000.0
+    if (model == "gpt-3.5-turbo") or (model == "gpt-3.5-turbo-1106"):
+        return (0.0015 * usage.prompt_tokens + 0.002 * usage.completion_tokens) / 1000.0
     if (model == "gpt-3.5-turbo") or (model == "gpt-3.5-turbo-1106"):
         return (0.0015 * usage.prompt_tokens + 0.002 * usage.completion_tokens) / 1000.0
 
